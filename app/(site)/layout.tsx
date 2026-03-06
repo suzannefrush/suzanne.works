5594dc14a414c2121d73c7d233ef24d73e1b0e43
suzannefrush <50508329+suzannefrush@users.noreply.github.com>
2026-03-06 10:29:05 -0500

    Remove DefaultLayout and use fragment instead

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,5 +1,3 @@
-import DefaultLayout from "@/components/PageLayouts/DefaultLayout";
-
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <DefaultLayout>{children}</DefaultLayout>;
+  return <>{children}</>;
 }
